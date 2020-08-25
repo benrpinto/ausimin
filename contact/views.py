94e5782d4eede02fe186079af245b52e381b6552
Benji <benrpinto@hotmail.com>
2020-08-25 21:45:56 +1000

    Form submission linked to url Starting to process submitted form

--- contact/views.py
+++ contact/views.py
@@ -6,4 +6,8 @@ def contact(request):
               'app_name':"Contact"
              } 
    return render(request, 'contact/contact.html', context)
+
+def mail(request):
+   print("form received")
+   return contact(request)
 # Create your views here.
