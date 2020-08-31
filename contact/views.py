46964364f8782b4be1b21177f206f6a9b48968d9
Benji <benrpinto@hotmail.com>
2020-08-31 20:03:44 +1000

    Handling when /contact/mail is called with GET (instead of POST)

--- contact/views.py
+++ contact/views.py
@@ -8,23 +8,24 @@ def contact(request):
    return render(request, 'contact/contact.html', context)
 
 def mail(request):
-   print("form received")
-   subject = "Message from "\
-   + request.POST['honorific'] +" "\
-   + request.POST['fName'] + " "\
-   + request.POST['lName']
-   message_body = "Company: " + request.POST['company']\
-   + "\n" + "Country: " + request.POST['country']\
-   + "\n" + "Email: "   + request.POST['emailAddress']\
-   + "\n" + "Phone: "   + request.POST['phone']\
-   + "\n" + "Reason: "  + request.POST['reason']\
-   + "\n" + "Message: " + request.POST['message']
-   send_mail(
-      subject,
-      message_body,
-      None,
-      ['ausimintest@mailinator.com'],
-      fail_silently=False,
-   )
+   if (request.method == "POST"):
+      print("form received")
+      subject = "Message from "\
+      + request.POST['honorific'] +" "\
+      + request.POST['fName'] + " "\
+      + request.POST['lName']
+      message_body = "Company: " + request.POST['company']\
+      + "\n" + "Country: " + request.POST['country']\
+      + "\n" + "Email: "   + request.POST['emailAddress']\
+      + "\n" + "Phone: "   + request.POST['phone']\
+      + "\n" + "Reason: "  + request.POST['reason']\
+      + "\n" + "Message: " + request.POST['message']
+      send_mail(
+         subject,
+         message_body,
+         None,
+         ['ausimintest@mailinator.com'],
+         fail_silently=False,
+      )
    return contact(request)
 # Create your views here.
