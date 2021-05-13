6fc8820d9e9f50725300ed25655d93300e5bb823
Benji <benrpinto@hotmail.com>
2021-05-13 22:08:53 +1000

    added uses to products page

--- products/views.py
+++ products/views.py
@@ -4,10 +4,16 @@ from .models import UsesContent
 from .models import CertContent
 
 def products(request):
-   myProducts = ProductContent.objects.all
-   myCerts    = CertContent.objects.all
+   myProducts = ProductContent.objects.all()
+   myCerts    = CertContent.objects.all()
+   myUses=[];
+   for counter in range(myProducts.count()):
+      myUses.append(UsesContent.objects.filter(related_product = myProducts[counter]))
+      counter = counter + 1
+
    context = {
               'prod_cont':myProducts,
+              'use_cont':myUses,
               'cert_cont':myCerts,
               'app_text':"Products",
               'app_name':"Products"
